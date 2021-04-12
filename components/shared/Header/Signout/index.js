3c727048a3db0d275a9cfed351ff2201d9257feb
Douglas <dsad>
2021-04-12 15:44:51 -0300

    init remove unused code

--- components/shared/Header/Signout/index.js
+++ components/shared/Header/Signout/index.js
@@ -1,11 +1,8 @@
 import React from "react";
-import cookie from "js-cookie";
-import { useRouter } from "next/router";
 
 import { SignoutContainer, StyledText } from "./styles";
 
 const Signout = () => {
-  const router = useRouter();
   return (
     <SignoutContainer>
       <StyledText onClick={() => {}}>Sair</StyledText>
